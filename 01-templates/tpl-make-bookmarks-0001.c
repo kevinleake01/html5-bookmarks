2eb8566070878a23991700f90e59ece3d10403f7
Kevin Leake <kevinleake01@gmail.com>
2020-07-01 11:46:58 +0000

    Add files via upload

---
+++ 01-templates/tpl-make-bookmarks-0001.c
@@ -0,0 +1,71 @@
+/*
+####################################
+#
+# --- TPL-MAKE-BOOKMARKS-0001.C ---
+#
+# This program makes an HTML5 Bookmark file.
+#
+# Example usage:
+#
+# gcc tpl-makebookmarks-0001.c; ./a.out 3 7 > tpl-bookmarks-3x7.html
+#
+####################################
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* --- Function Prototypes go here --- */
+int main(int argc, char *argv[]);
+
+/* --- Global variables go here --- */
+
+int main(int argc, char *argv[])
+{
+  int num1, num2, subhead, entries;
+  
+  subhead=atoi(argv[1]);
+  entries=atoi(argv[2]);
+  
+  printf("<!DOCTYPE html>\n");
+  printf("<html lang=\"en\">\n");
+  printf("<head>\n");
+  printf("<meta charset=\"utf-8\" >\n");
+  printf("<title>HTML5 Bookmarks</title>\n");
+  printf("<style>\n");
+  printf("body {\n");
+  printf("  background-color: #000000;\n");
+  printf("  color: #FFFFFF;\n");
+  printf("}\n");
+  printf("h1 {\n");
+  printf("  background-color: #585858;\n");
+  printf("  color: #FFFFFF;\n");
+  printf("}\n");
+  printf("h2 {\n");
+  printf("  background-color: #585858;\n");
+  printf("  color: #FFFFFF;\n");
+  printf("}\n");
+  printf("a:link {\n");
+  printf("  color: #FF0000;\n");
+  printf("}\n");
+  printf("a:visited {\n");
+  printf("  color: #00FF00;\n");
+  printf("}\n");
+  printf("</style>\n");
+  printf("</head>\n");
+  printf("<body>\n");
+  printf("<h1>HTML5 Bookmarks</h1>\n");
+  for (num1=1; num1<=subhead; num1++)
+  {
+    printf("<h2>Sub Heading #%02d</h2>\n", num1);
+    printf("<p>\n");
+    for (num2=1; num2<=entries; num2++)
+    {
+      printf("<a href=\"-\">-</a><br>\n");
+    }
+    printf("</p>\n");
+  }
+  printf("</body>\n");
+  printf("</html>\n");
+}
+
